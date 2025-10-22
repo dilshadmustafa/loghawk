915a9430e570de33130ff2b3711b1e20e51c9e8b
Dilshad Mustafa <mdilshad2016@yahoo.com>
2025-10-22 20:17:22 +0530

    update

--- security_docs_to_ragvectordb/upload_security_doc.py
+++ security_docs_to_ragvectordb/upload_security_doc.py
@@ -19,20 +19,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
 from lancedb.embeddings import get_registry
 from lancedb.pydantic import LanceModel, Vector
-
 from loghawk_utils.lancedbutils import init_database
-#import lancedb
-
-# def init_database(db_file, table_name):
-#     """Initialize the database."""
-#     # Create a LanceDB table with the schema
-#     db = lancedb.connect(db_file)
-#
-#     if table_name not in db.table_names():
-#         table = db.create_table(table_name, schema=MyTableSchema)
-#     else:
-#         table = db.open_table(table_name)
-#     return db, table
 
 # Get a sentence-transformer function
 func = get_registry().get("sentence-transformers").create()
